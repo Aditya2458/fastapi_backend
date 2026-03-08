d214f7b60ba41cbe092b919d06fbc0c675a5fc63
Aditya Sahu <adityacbsa29@gmail.com>
2026-03-09 00:39:40 +0530

    feat: add subjects API and test coverage

--- tests/test_marks.py
+++ tests/test_marks.py
@@ -3,41 +3,62 @@ import uuid
 
 def test_teacher_can_add_marks(client):
 
-    # create teacher
-    email = f"teacher_{uuid.uuid4()}@gmail.com"
     password = "Strong@123"
 
+    # create admin
+    admin_email = f"admin_{uuid.uuid4()}@gmail.com"
+
     client.post("/api/auth/signup", json={
-        "name": "Teacher",
-        "age": 40,
+        "name": "Admin",
+        "age": 30,
         "location": "Delhi",
-        "email": email,
-        "role": "teacher",
+        "email": admin_email,
+        "role": "admin",
         "password": password,
         "confirm_password": password
     })
 
-    login = client.post("/api/auth/login", json={
-        "email": email,
+    admin_login = client.post("/api/auth/login", json={
+        "email": admin_email,
         "password": password
     })
 
-    token = login.json()["access_token"]
+    admin_token = admin_login.json()["access_token"]
 
-    # create subject first
+    # create subject
     subject_code = f"SUB_{uuid.uuid4().hex[:6]}"
 
-    subject = client.post(
+    client.post(
         "/api/subjects/",
         json={
             "name": "Physics",
             "code": subject_code
         },
-        headers={"Authorization": f"Bearer {token}"}
+        headers={"Authorization": f"Bearer {admin_token}"}
     )
 
     subjects = client.get("/api/subjects/")
-    subject_id = subjects.json()[-1]["id"]
+    subject_id = subjects.json()[0]["id"]
+
+    # create teacher
+    teacher_email = f"teacher_{uuid.uuid4()}@gmail.com"
+
+    client.post("/api/auth/signup", json={
+        "name": "Teacher",
+        "age": 40,
+        "location": "Delhi",
+        "email": teacher_email,
+        "role": "teacher",
+        "password": password,
+        "confirm_password": password
+    })
+
+    teacher_login = client.post("/api/auth/login", json={
+        "email": teacher_email,
+        "password": password
+    })
+
+    teacher_token = teacher_login.json()["access_token"]
 
     # create student
     student_email = f"student_{uuid.uuid4()}@gmail.com"
@@ -52,7 +73,6 @@ def test_teacher_can_add_marks(client):
         "confirm_password": password
     })
 
-    # student record already auto-created in your system
     student_id = 1
 
     response = client.post(
@@ -63,7 +83,7 @@ def test_teacher_can_add_marks(client):
             "exam_type": "Midterm",
             "marks": 90
         },
-        headers={"Authorization": f"Bearer {token}"}
+        headers={"Authorization": f"Bearer {teacher_token}"}
     )
 
     assert response.status_code == 200
